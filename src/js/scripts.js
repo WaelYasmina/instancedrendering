d5d8b0b0b527e67b5440e68c932c52f20e983c19
WaelYasmina <waelyasmina@outlook.com>
2024-11-20 22:11:38 +0100

    Update scripts.js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,122 +1,105 @@
 import * as THREE from 'three';
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
-import {RGBELoader} from 'three/examples/jsm/loaders/RGBELoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
 
-const renderer = new THREE.WebGLRenderer({antialias: true});
-
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
-
 document.body.appendChild(renderer.domElement);
 
+// Sets the color of the background.
+renderer.setClearColor(0xfefefe);
+
 const scene = new THREE.Scene();
-
-renderer.setClearColor(0xFFEA00);
-
 const camera = new THREE.PerspectiveCamera(
-    45,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
+  45,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
 );
 
-camera.position.set(0, 6, 15);
-camera.lookAt(scene.position);
+// Sets orbit control to move the camera around.
+const orbit = new OrbitControls(camera, renderer.domElement);
 
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.update();
+// Camera positioning.
+camera.position.set(6, 8, 14);
+// Has to be done everytime we update the camera position.
+orbit.update();
 
-const ambientLight = new THREE.AmbientLight(0x333333);
-scene.add(ambientLight);
+// const ambientLight = new THREE.AmbientLight(0x333333);
+// scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1);
-directionalLight.position.set(0, 10, 10);
-scene.add(directionalLight);
+// const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+// directionalLight.position.set(0, 10, 10);
+// scene.add(directionalLight);
+
+// const geometry = new THREE.IcosahedronGeometry();
+// const material = new THREE.MeshPhongMaterial();
+// const mesh = new THREE.InstancedMesh(geometry, material, 10000);
+// scene.add(mesh);
 
 const gltfLoader = new GLTFLoader();
 const rgbeLoader = new RGBELoader();
 
-renderer.outputEncoding = THREE.sRGBEncoding;
-renderer.toneMapping = THREE.ACESFilmicToneMapping;
-renderer.toneMappingExposure = 2;
+let starMesh;
+const dummy = new THREE.Object3D();
 
-rgbeLoader.load('./assets/MR_INT-005_WhiteNeons_NAD.hdr', function(texture) {
-    texture.mapping = THREE.EquirectangularReflectionMapping;
-    scene.environment = texture;
+rgbeLoader.load('/rosendal_plains_1_1k.hdr', function (texture) {
+  texture.mapping = THREE.EquirectangularReflectionMapping;
+  scene.environment = texture;
 
-    gltfLoader.load('./assets/star.glb', function(glb) {
-        const mesh = glb.scene.getObjectByName('Star_Star_0');
-        const geometry = mesh.geometry.clone();
-        const material = mesh.material;
-        const starMesh = new THREE.InstancedMesh(geometry, material, 10000);
-        scene.add(starMesh);
+  gltfLoader.load('/star.glb', function (glb) {
+    const mesh = glb.scene.getObjectByName('Star_Star_0');
+    const geometry = mesh.geometry.clone();
+    const material = mesh.material;
 
-        const dummy = new THREE.Object3D();
-        for(let i = 0; i < 10000; i++) {
-            dummy.position.x = Math.random() * 40 - 20;
-            dummy.position.y = Math.random() * 40 - 20;
-            dummy.position.z = Math.random() * 40 - 20;
+    starMesh = new THREE.InstancedMesh(geometry, material, 10000);
+    scene.add(starMesh);
 
-            dummy.rotation.x = Math.random() * 2 * Math.PI;
-            dummy.rotation.y = Math.random() * 2 * Math.PI;
-            dummy.rotation.z = Math.random() * 2 * Math.PI;
+    for (let i = 0; i < 10000; i++) {
+      dummy.position.x = Math.random() * 40 - 20;
+      dummy.position.y = Math.random() * 40 - 20;
+      dummy.position.z = Math.random() * 40 - 20;
 
-            dummy.scale.x = dummy.scale.y = dummy.scale.z = 0.04 * Math.random();
+      dummy.rotation.x = Math.random() * 2 * Math.PI;
+      dummy.rotation.y = Math.random() * 2 * Math.PI;
+      dummy.rotation.z = Math.random() * 2 * Math.PI;
 
-            dummy.updateMatrix();
-            starMesh.setMatrixAt(i, dummy.matrix);
-            starMesh.setColorAt(i, new THREE.Color(Math.random() * 0xFFFFFF));
-        }
-    });
+      dummy.scale.x = dummy.scale.y = dummy.scale.z = 0.04 * Math.random();
+
+      dummy.updateMatrix();
+      starMesh.setMatrixAt(i, dummy.matrix);
+      starMesh.setColorAt(i, new THREE.Color(Math.random() * 0xffffff));
+    }
+  });
 });
 
-// const geometry = new THREE.IcosahedronGeometry();
-// const material = new THREE.MeshPhongMaterial({color: 0xFFEA00});
-// const mesh = new THREE.InstancedMesh(geometry, material, 10000);
-// scene.add(mesh);
-
-// const dummy = new THREE.Object3D();
-// for(let i = 0; i < 10000; i++) {
-//     dummy.position.x = Math.random() * 40 - 20;
-//     dummy.position.y = Math.random() * 40 - 20;
-//     dummy.position.z = Math.random() * 40 - 20;
-
-//     dummy.rotation.x = Math.random() * 2 * Math.PI;
-//     dummy.rotation.y = Math.random() * 2 * Math.PI;
-//     dummy.rotation.z = Math.random() * 2 * Math.PI;
-
-//     dummy.scale.x = dummy.scale.y = dummy.scale.z = Math.random();
-
-//     dummy.updateMatrix();
-//     mesh.setMatrixAt(i, dummy.matrix);
-//     mesh.setColorAt(i, new THREE.Color(Math.random() * 0xFFFFFF));
-// }
-
-// const matrix = new THREE.Matrix4();
+const matrix = new THREE.Matrix4();
 function animate(time) {
-    // for(let i = 0; i < 10000; i++) {
-    //     mesh.getMatrixAt(i, matrix);
-    //     matrix.decompose(dummy.position, dummy.rotation, dummy.scale);
+  if (starMesh) {
+    for (let i = 0; i < 10000; i++) {
+      starMesh.getMatrixAt(i, matrix);
+      matrix.decompose(dummy.position, dummy.rotation, dummy.scale);
 
-    //     dummy.rotation.x = i/10000 * time/1000;
-    //     dummy.rotation.y = i/10000 * time/500;
-    //     dummy.rotation.z = i/10000 * time/1200;
-    
-    //     dummy.updateMatrix();
-    //     mesh.setMatrixAt(i, dummy.matrix);
-    // }
-    // mesh.instanceMatrix.needsUpdate = true;
+      dummy.rotation.x = ((i / 10000) * time) / 1000;
+      dummy.rotation.y = ((i / 10000) * time) / 500;
+      dummy.rotation.z = ((i / 10000) * time) / 1200;
 
-    // mesh.rotation.y = time / 10000;
+      dummy.updateMatrix();
+      starMesh.setMatrixAt(i, dummy.matrix);
+    }
+    starMesh.instanceMatrix.needsUpdate = true;
 
-    renderer.render(scene, camera);
+    starMesh.rotation.y = time / 10000;
+  }
+
+  renderer.render(scene, camera);
 }
 
 renderer.setAnimationLoop(animate);
 
-window.addEventListener('resize', function() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+window.addEventListener('resize', function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
 });
